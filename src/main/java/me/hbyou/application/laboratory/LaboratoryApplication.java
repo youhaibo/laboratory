567a40fb4b2c8d53c58fe47845236e01d29f721d
尤淇 <youqi.yhb@alibaba-inc.com>
2017-08-05 15:03:02 +0800

    init

---
+++ src/main/java/me/hbyou/application/laboratory/LaboratoryApplication.java
@@ -0,0 +1,15 @@
+package me.hbyou.application.laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Tony
+ */
+@SpringBootApplication
+public class LaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryApplication.class, args);
+	}
+}
