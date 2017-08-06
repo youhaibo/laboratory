e2aef10f3c97abf9f0e0e2366ee4d50ff2d71287
YouHaibo <hbyou21@gmail.com>
2017-08-06 16:45:36 +0800

    ok

--- src/main/java/me/hbyou/application/laboratory/http/HttpClientFactory.java
+++ src/main/java/me/hbyou/application/laboratory/http/HttpClientFactory.java
@@ -1,7 +1,14 @@
 package me.hbyou.application.laboratory.http;
 
+import java.io.IOException;
+
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Tony(YouHaibo)
@@ -9,10 +16,81 @@ import org.apache.http.impl.client.HttpClients;
  */
 public class HttpClientFactory {
 
-    public HttpClient getHttpClient() {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpClientFactory.class);
 
-        HttpClient httpClient = HttpClients.createDefault();
+    private static volatile CloseableHttpClient httpClient;
 
+    /**
+     * 连接池最大连接数
+     */
+    private static final Integer MAX_TOTAL = 1000;
+
+    /**
+     * 每个route默认的最大连接数
+     */
+    private static final Integer DEFAULT_MAX_PER_ROUTE = 200;
+
+    /**
+     * 从连接池中获取连接的超时时间，超过该时间未拿到可用连接，
+     * 会抛出org.apache.http.conn.ConnectionPoolTimeoutException:
+     * Timeout waiting for connection from pool
+     */
+    private static final Integer CONNECTION_REQUEST_TIMEOUT = 3000;
+    /**
+     * 连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
+     */
+    private static final Integer CONNECT_TIMEOUT = 10000;
+
+    /**
+     * 服务器返回数据(response)的时间，超过该时间抛出read timeout
+     */
+    private static final Integer SOCKET_TIMEOUT = 60000;
+
+    /**
+     * 空闲永久连接检查间隔
+     * 官方推荐使用这个来检查永久链接的可用性，而不推荐每次请求的时候才去检查
+     */
+    private static final Integer VALIDATE_AFTER_INACTIVITY = 2000;
+
+    private HttpClientFactory() {
+    }
+
+    /**
+     * 获取HttpClient对象
+     */
+    public static CloseableHttpClient getHttpClient() {
+        if (httpClient == null) {
+            synchronized (HttpClientFactory.class) {
+                if (httpClient == null) {
+                    PoolingHttpClientConnectionManager ppccm = new PoolingHttpClientConnectionManager();
+                    ppccm.setMaxTotal(MAX_TOTAL);
+                    ppccm.setDefaultMaxPerRoute(DEFAULT_MAX_PER_ROUTE);
+                    ppccm.setValidateAfterInactivity(VALIDATE_AFTER_INACTIVITY);
+                    RequestConfig requestConfig = RequestConfig.custom()
+                        .setConnectionRequestTimeout(CONNECTION_REQUEST_TIMEOUT)
+                        .setConnectTimeout(CONNECT_TIMEOUT)
+                        .setSocketTimeout(SOCKET_TIMEOUT)
+                        .build();
+
+                    httpClient = HttpClients.custom()
+                        .setDefaultRequestConfig(requestConfig)
+                        .setConnectionManager(ppccm)
+                        .build();
+
+                }
+            }
+        }
         return httpClient;
+    }
+
+    public static void close() {
+        if (httpClient != null) {
+            try {
+                httpClient.close();
+                httpClient = null;
+            } catch (IOException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
     }
 }
---
+++ src/main/java/me/hbyou/application/laboratory/http/HttpUtils.java
@@ -0,0 +1,120 @@
+package me.hbyou.application.laboratory.http;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Tony(YouHaibo)
+ * @date 2017-08-06 16:32
+ */
+public class HttpUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtils.class);
+
+    /**
+     * 默认的重试次数
+     */
+    private static final Integer DEFAULT_RETYR_TIMES = 2;
+
+    /**
+     * 默认的重试间隔
+     */
+    private static final Long DEFAULT_RETRY_INTERVAL = 3000L;
+
+    public static String xmlPostWithRetry(String url, String xmlParams) {
+        return xmlPostWithRetry(url, xmlParams, DEFAULT_RETYR_TIMES, DEFAULT_RETRY_INTERVAL);
+    }
+
+    /**
+     * 带重试机制的http post请求工具
+     *
+     * @param url        请求的url
+     * @param xmlParams  xml格式的参数
+     * @param retryTimes 重试次数
+     * @param interval   重试时间间隔
+     *                   这里实际等待的时间为：interval*retryTimes
+     * @return 返回响应内容
+     * null代表错误
+     */
+    public static String xmlPostWithRetry(String url, String xmlParams, int retryTimes, long interval) {
+        int i = 0;
+        while (i < retryTimes) {
+            String response = xmlPost(url, xmlParams);
+            if (StringUtils.isNotBlank(response)) {
+                return response;
+            }
+
+            i++;
+
+            takeABreak(interval * i);
+        }
+
+        return null;
+    }
+
+    /**
+     * post请求支持
+     */
+    public static String xmlPost(String url, String xmlParams) {
+        if (StringUtils.isBlank(url)) {
+            return null;
+        }
+        CloseableHttpClient httpclient = HttpClientFactory.getHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        if (StringUtils.isNotBlank(xmlParams)) {
+            HttpEntity entity = new ByteArrayEntity(xmlParams.getBytes(StandardCharsets.UTF_8));
+            httpPost.setEntity(entity);
+        }
+        try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+            int statuCode = response.getStatusLine().getStatusCode();
+            HttpEntity responseEntity = response.getEntity();
+            String responseContent = "";
+            if (responseEntity != null) {
+                responseContent = EntityUtils.toString(responseEntity);
+
+                EntityUtils.consume(responseEntity);
+            }
+            return responseContent;
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    public static String post(String url) {
+        CloseableHttpClient httpclient = HttpClientFactory.getHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        String responseContent = "";
+        try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+            int statuCode = response.getStatusLine().getStatusCode();
+            HttpEntity responseEntity = response.getEntity();
+
+            if (responseEntity != null) {
+                responseContent = EntityUtils.toString(responseEntity);
+
+                EntityUtils.consume(responseEntity);
+            }
+            return responseContent;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return responseContent;
+    }
+
+    private static void takeABreak(long mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+}
